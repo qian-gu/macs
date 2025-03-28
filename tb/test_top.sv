9c8fc400f038d8a169cd623c78dc2a669357775d
Qian Gu <imqiangu@gmail.com>
2025-03-28 08:25:31 +0800

    chore: support generate gaussian dist data in tb

--- tb/test_top.sv
+++ tb/test_top.sv
@@ -34,9 +34,9 @@ module test_top;
       $finish;
     end else begin
       #0.1;
-      $display("cnt = %0d: A * W = %0d * %0d = %0d = %0d",
-        cnt, $signed(MAC_TOP.A_q), $signed(MAC_TOP.W_q),
-        $signed(MAC_TOP.Q_baseline), $signed(MAC_TOP.Q_mm));
+      //$display("cnt = %0d: A * W = %0d * %0d = %0d = %0d",
+        //cnt, $signed(MAC_TOP.A_q), $signed(MAC_TOP.W_q),
+        //$signed(MAC_TOP.Q_baseline), $signed(MAC_TOP.Q_mm));
     end
     if (cnt == MAX) begin
       $display("Success!");
@@ -71,4 +71,45 @@ module test_top;
     $dumpvars;
   end
 
+  function int get_gaussian(int seed, real mean, real std_dev);
+
+    real raw_value;
+    int result;
+
+    do begin
+      raw_value = $dist_normal(seed, mean, std_dev);
+      result = int'(raw_value);
+    end while (result < -128 || result > 127);
+
+    return result;
+
+  endfunction  // get_gaussian
+
+  int seed;
+  int value;
+  real mean;
+  real std_dev;
+  real histgram[-128:127];
+
+  initial begin
+    mean = 0;
+    std_dev = 42.7;
+
+    for (int i = -128; i <= 127; i++) begin
+      histgram[i] = 0;
+    end
+
+    repeat (1000) begin
+      seed= $time + $urandom;
+      value = get_gaussian(seed, mean, std_dev);
+      histgram[value]++;
+    end
+
+    for (int i = -128; i <= 127; i++) begin
+      $display("Value %4d: %4d occurence", i, histgram[i]);
+    end
+  end
+
 endmodule  // test_top;
+
+
